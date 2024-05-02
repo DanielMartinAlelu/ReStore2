22c8040c16a0e9735ef64dd42bad436217c20868
Daniel Martin Alelu <Daniel.Martin-Alelu@healthdiagnostics.co.uk>
2024-05-02 13:07:02 +0100

    Add project files.

---
+++ API/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; } // Chosen long for a specific reason based on the payment
+        public string  PictureUrl { get; set; }
+        public string Type { get; set;}
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
