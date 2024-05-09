edfe2021ff54543f0a1df2efeea6d710fc141211
Daniel Martin Alelu <Daniel.Martin-Alelu@healthdiagnostics.co.uk>
2024-05-09 14:37:21 +0100

    Adding cards and styles for products

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Button } from "@mui/material";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 // create an interface to be able to give the type Product (we must import it)
 interface Props {
     products: Product[];
@@ -10,16 +11,7 @@ interface Props {
 export default function Catalog({ products, addProduct }: Props) {
     return (
         <>
-            <List>
-                {products.map(product => (
-                    <ListItem key={product.id}>
-                        <ListItemAvatar>
-                            <Avatar src={product.pictureUrl}></Avatar>
-                        </ListItemAvatar>
-                        <ListItemText>{product.name} - {product.price}</ListItemText>
-                    </ListItem>
-                ))}
-            </List>
+            <ProductList products={products} />
             <Button variant='contained' onClick={addProduct}>Add product</Button>
         </>
     )
