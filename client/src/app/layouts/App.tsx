30fa19d7de0b43f15caba5e49dfbc0c21dc8ed7d
Daniel Martin Alelu <Daniel.Martin-Alelu@healthdiagnostics.co.uk>
2024-05-09 15:48:33 +0100

    Added dark mode

--- client/src/app/layouts/App.tsx
+++ client/src/app/layouts/App.tsx
@@ -1,40 +1,32 @@
-import { useEffect, useState } from "react"
-import { Product } from "../models/product";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
-    // save the list in the function to use it out of this file
-    const [products, setProducts] = useState<Product[]>([]);
+    const [darkMode, setDarkMode] = useState(false);
+    const paletteType = darkMode ? 'dark' : 'light';
+    const theme = createTheme({
+        palette: {
+            mode: paletteType,
+            background: {
+                default: paletteType === 'light' ? '#eaeaea' : '#121212'
+            }
+        }
+    })
 
-    // hooks
-    // Get data from API, using JS
-    useEffect(() => {
-        fetch('http://localhost:5000/api/products').then(response => response.json()).then(data => setProducts(data))
-    }, []) // an empty array[] in the 'dependencies' prevent a endless loop
-
-    // React components (v23)
-    function addProduct() {
-        setProducts(prevState => [...prevState,
-        {
-            id: prevState.length + 101,
-            name: 'product' + (prevState.length + 1),
-            price: (prevState.length * 100) + 100,
-            brand: 'some brand',
-            description: 'some description',
-            pictureUrl: 'http://picsum.photos/200'
-        }])
+    function handleThemeChange() {
+        setDarkMode(!darkMode);
     }
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Header />
+            <Header darkMode={darkMode} handleThemeChange={handleThemeChange } />
             <Container>
-                <Catalog products={products} addProduct={addProduct} />
+                <Catalog />
             </Container>
-        </>
+        </ThemeProvider>
     )
 }
 
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Button } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
 // create an interface to be able to give the type Product (we must import it)
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
-export default function Catalog({ products, addProduct }: Props) {
+
+export default function Catalog() {
+    // save the list in the function to use it out of this file
+    const [products, setProducts] = useState<Product[]>([]);
+
+    // hooks
+    // Get data from API, using JS
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products').then(response => response.json()).then(data => setProducts(data))
+    }, []) // an empty array[] in the 'dependencies' prevent a endless loop
+
+    // React components (v23)
+
+
     return (
         <>
             <ProductList products={products} />
-            <Button variant='contained' onClick={addProduct}>Add product</Button>
         </>
     )
 }
