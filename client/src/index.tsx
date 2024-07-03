628118f100fbe1ff3c530fd85f4890d465daeb8d
Daniel Martin Alelu <Daniel.Martin-Alelu@healthdiagnostics.co.uk>
2024-07-03 15:07:06 +0100

    Section 7 - Some work with Redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,14 +9,19 @@ import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes.js';
 import { StoreProvider } from './app/context/StoreContext.js';
+import { configureStore } from './app/store/configureStore.js';
+import { Provider } from 'react-redux';
 
+const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         {/* instead load the app component: <App />, we swap that for router provider */}
         <StoreProvider>
+            <Provider store={store}>
             <RouterProvider router={router} />
+            </Provider>
         </StoreProvider>
     </React.StrictMode>,
 )
